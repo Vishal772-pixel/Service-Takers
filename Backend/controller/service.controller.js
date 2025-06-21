cadc0096b2d098bfeb64f43d9275c0ba1366682f
Vishal772-pixel <fantasydark001@gmail.com>
2025-06-21 16:05:34 +0530

    feat: Implement create service functionality with validation

---
+++ Backend/controller/service.controller.js
@@ -0,0 +1,33 @@
+// Create service 
+// Get service 
+
+import express from "express";
+import mongoose from "mongoose";
+import serviceSchema from "../model/services.model.js";
+
+const createService = async(req,res)=> {
+    try {
+        const{title,description,category,price,deliveryTime,features} =req.body; 
+        if(!title||!description||!category||!price||!deliveryTime||!features){
+            return res.status(400).json({message:"All fields are required"});
+        }
+        const newService = new serviceSchema({
+            title,
+            description,
+            category,
+            price,
+            deliveryTime,
+            features
+        })
+        await newService.save();
+        res.status(201).json({message:"Service created successfully",service:newService});
+
+        
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"Internal server error"});
+        
+    }
+}
+
